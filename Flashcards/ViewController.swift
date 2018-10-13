044655006c361195e3eb7da65d617e80956dbdea
Steven Marmolejo <Freesoap1314@stevens-macbook-pro.local>
2018-10-13 16:09:47 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Steven Marmolejo on 10/13/18.
+//  Copyright © 2018 Steven Marmolejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
